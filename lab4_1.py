936c2a2f019b6ce3f2906e8366eb10018d09084f
Jennifer Espinoza <jenny@Jennifer-Espinozas-MacBook-2.local>
2020-10-11 14:41:27 -0700

    finished

--- lab4_1.py
+++ lab4_1.py
@@ -7,7 +7,11 @@ def is_even():
    # TODO: 
    # Create a conditional that sets out to True if the user enters
    # an even integer and False if the user enters an odd integer.
-    
+    if user_data % 2 == 0:
+       out = True
+       else:
+          out = False
+
    # This is for the test.
    return out 
 
@@ -16,14 +20,23 @@ def multi_condition():
    # in Everyday Coding, we'll learn how to catch errors like this.
    user_data = int(input("Please give me an integer. "))
 
-   #TODO:
-   # Now evaluate the user_data again and have several conditions
-   # if the value is zero, print "Don't be such a zero!"
-   # if the value is positive and odd, print "Positively odd!"
-   # if the value is positive and even, print "Even Steven!"
-   # if the value is negative and odd or even, print "Negative Nelly!"
-   # remember, with multiple conditions you can use if, elif, elif, else
    
+   if user_data == 0:
+      print("Don't be such a zero!")
+      elif user_data < 0:
+         print("Negative Nelly!")
+   elif user_data < 0:
+   if user_data > 0 and user_data % 2 !=0
+      print("Positively odd!")
+   user_data > 0 and user_data % 2== ):
+      print("Even Steven!")
+      else:
+         print("Positively odd!")
+   user_data < 0 and (user_data % 2 == 0 or user_data % 2 ! - 0):
+      print("Negative Nelly!")
+   if user_data == 0:
+      print("Don't be such a zero!")
+
    return None
 
 
@@ -31,18 +44,22 @@ def is_underage():
    #TODO:
    # Prompt the user for their age and store it to a variable. Don't
    # forget to convert the input to an integer.
+  user_data = int(input("How old are you? "))
 
-   #TODO:
-   # Set up a conditional with four cases
    # if the age is equal to or above 21, 
-   # print "You may drink, smoke, and drive if you wish!"
+   if user_data >=21:
+      print("You may drink, smoke, and drive if you wish!")
    # if the age is equal to or above 18 but less than 21, 
-   # print "You may smoke and drive!"
+   elif user_data >=18:
+      print ("You may smoke and drive!")
    # if the age is equal to or above 16 but less than 18,
-   # print "You may drive!"
+  elif user_data >= 16:
+     print("You may drive!")
+
+  
    # if the age is less than 16, 
-   # print "Enjoy your bike, kid!"
-   
+   else:
+      print("Enjoy your bike, kid!")
    # So the tests fail and they don't throw errors
    return None
 
@@ -51,6 +68,8 @@ def countdown():
    # Use a loop to print a countdown from 10 to zero with
    # one number on each new line. If you do not use a loop
    # you will not get points for this problem.
+   for i in range(10,-1,-1):
+      print(i)
 
    # So the tests fail and they don't throw errors
    return None
@@ -71,6 +90,28 @@ def guessing_game(num):
    # if the user enters that the program should exit, saying 
    # "Goodbye, quitter!"
    # No, it's not a very nice program. 
+
+   # I need to get user input 
+   # give the user 10 guesses
+   # i need to check user input 
+   # Check if user says quit
+   counter = 0 
+   end game = False
+   while counter < 10 and not end_game:
+      user_input = int(input("Guess a number, please! (Q to quit)")  
+      if user_input.lower() == "q":
+         #somehow break the while
+         end_game = True 
+         print("Goodbye, quitter!")
+         elif user_input == num:
+            end_game= True
+            print("You win!")
+            elif user_input < num:
+         print("Too Low!")
+      else:
+         user_input - int(user_input)
+         print("Too High!")
+         counter += 1
 
 
    # So the tests fail and they don't throw errors
